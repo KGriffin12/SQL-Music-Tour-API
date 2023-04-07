abd2d80a3a3ad5563fe891ca70f51e86665b64dc
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-04-06 22:07:05 -0500

    Sequelized and tested.

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
-
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: process.env.PG_PW
+  })
+  
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
