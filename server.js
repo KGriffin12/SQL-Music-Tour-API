89dcfe6ba60e98bf687a8f24822b68bf1d4a5bb0
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-04-13 17:04:27 -0500

    completed event_controller.

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: process.env.PG_PW
-  })
-  
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
